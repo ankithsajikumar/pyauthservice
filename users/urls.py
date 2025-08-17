b124aee5ef3a6952acab8d486e464eaa43aae372
ankit <ankithss774@gmail.com>
2025-08-17 19:26:53 +0530

    Adding me view for authorised users

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserListCreateView, UserRetrieveUpdateDestroyView
+from .views import UserListCreateView, UserRetrieveUpdateDestroyView, MeView
 
 urlpatterns = [
     path('users/', UserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
+    path('auth/me/', MeView.as_view(), name='me'),
 ]

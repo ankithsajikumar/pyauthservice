279418e1b6abe717a97de014de603138cee69e0c
ankit <ankithss774@gmail.com>
2025-08-23 00:48:29 +0530

    Adding login and logout endpoints for native auth

--- pyauthservice/urls.py
+++ pyauthservice/urls.py
@@ -1,18 +1,3 @@
-"""pyauthservice URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from oauth2_provider import urls as oauth2_urls
@@ -21,7 +6,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import home_redirect, status_report, login_page
+from .views import home_redirect, status_report, login_page, login_view, logout_view
 
 urlpatterns = [
     path('', home_redirect, name='home-redirect'),
@@ -31,6 +16,8 @@ urlpatterns = [
     path('api/', include(users_urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/login/', login_view, name='api_login'),
+    path('api/logout/', logout_view, name='api_logout'),
     path('api/status/', status_report, name='status-report'),
 ]
 

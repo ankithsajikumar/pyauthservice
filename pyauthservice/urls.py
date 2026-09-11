a9f0dde7f34ecc64e2a15cc1e34d149b94acf7a1
ankit <ankithss774@gmail.com>
2026-09-12 02:51:22 +0530

    Moving oauth to root

--- pyauthservice/urls.py
+++ pyauthservice/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 from oauth2_provider import urls as oauth2_urls
 from users import urls as users_urls
@@ -11,14 +11,15 @@ from .views import login_view, logout_view, health
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/admin/', permanent=False), name='home'),
+    path('', include(oauth2_urls)),
     path('admin/', admin.site.urls),
-    path('o/', include(oauth2_urls)),
     path('api/', include(users_urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/login/', login_view, name='api_login'),
     path('api/logout/', logout_view, name='api_logout'),
     path('health/', health, name='health'),
+    re_path(r'^.*$', RedirectView.as_view(url='/admin/', permanent=False)),
 ]
 
 admin.site.site_header = 'Auth Service Administration'
